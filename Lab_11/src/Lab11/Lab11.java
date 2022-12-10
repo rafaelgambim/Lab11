6ab26094938be4b0edf61a50cec277f57d270e3f
rafaelgambim <rafaelgambim15@gmail.com>
2022-12-09 20:37:13 -0500

    First Commit

---
+++ Lab_11/src/Lab11/Lab11.java
@@ -0,0 +1,22 @@
+package Lab11;
+import java.util.Scanner;
+
+public class Lab11 {
+   
+   public static int fibonacci(int n) {
+   if(n<0)
+return -1;
+else if (n <= 1)
+return n;
+return fibonacci(n-1) + fibonacci(n-2);
+    
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int startNum;
+      
+      startNum = scnr.nextInt();
+      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+   }
+}
